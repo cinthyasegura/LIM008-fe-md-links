7325ed7a9167cbb17ac85b9ab88848e1d8fbc4a6
cinthyasegura <cinthyaless@gmail.com>
2019-03-10 23:24:30 -0500

    se corrige el bug al exportar el modulo

--- src/cli.js
+++ src/cli.js
@@ -6,26 +6,28 @@ const args = process.argv.slice(2);
 const options = {
   validate: false,
   stats: false
-
 };
-const path = args[0];
+
+let path = args[0];
 
 if (args.length === 0) {
-  console.log('Ingresa una ruta, ejemplo: md-links ./some/example \n');
+  console.log('Ingresa una ruta, ejemplo: md-links ./some/example\n');
 }
 
-// const message = () => {
-//   console.log(`\n Uso: \n\n$ md-links <route> <options> \n\n<route> es la ruta del archivo o carpeta a evaluar \n<options> tendrán los valores de:
-// --stats, muestra cantidad de links y cantidad de links únicos \n--validate, muestra ruta de archivo, texto de referencia, link, estado de link \n--stats --validate, muestra cantidad de links, cantidad de links únicos y cantidad de links rotos`);
-// }
+const helpMe = () => {
+  console.log(`\n Uso: \n\n$ md-links <route> <options> \n\n<route> es la ruta del archivo o carpeta a evaluar \n<options> tendrán los valores de:
+--stats, muestra cantidad de links y cantidad de links únicos \n--validate, muestra ruta de archivo, texto de referencia, link, estado de link \n--stats --validate, muestra cantidad de links, cantidad de links únicos y cantidad de links rotos\n`);
+};
 
 // si solo ingresa la ruta
 if (args.length === 1) {
-  validateAndStats(path, options);
-}
-// if (args[0] === '--help') {
-//   message();
-// }
+  if (args[0] === '--help') {
+    helpMe();
+  } else {
+    validateAndStats(path, options);
+  }    
+};
+
 
 if (args.length === 2) {
   if (args[1] === '--validate' || args[1] === '--v') {
@@ -35,7 +37,7 @@ if (args.length === 2) {
     options.stats = true;
     validateAndStats(path, options);
   } 
-} 
+}; 
 
 if (args.length === 3) {
   if ((args[1] === '--validate' || args[1] === '--v') && (args[2] === '--stats' || args[2] === '--s')) {
@@ -47,4 +49,4 @@ if (args.length === 3) {
     options.stats = true;
     validateAndStats(path, options);
   }
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import { totalLinksStats, uniqueLinksStats, brokenLinksStats } from './controller/stats.js';
 import { validateLink } from './controller/validate.js';
 
-
-// const options = {
-//   validate: true,
-//   stats: true
-// }; 
-
-
 export const validateAndStats = (route, options) => {
   let promises = 0;
   if (options.stats && !options.validate) {
@@ -29,12 +22,10 @@ export const validateAndStats = (route, options) => {
   } else {
     validateLink(route)
       .then((resp) => {
-        resp.forEach(values => console.log(` Path: ${values.file}\n Link: ${values.href}\n Text: ${values.text}`));
+        resp.forEach(values => console.log(` Path: ${values.file}\n Link: ${values.href}\n Text: ${values.text}\n`));
       }).catch(err => err);
   }
   return promises;
 };
-  
-// validateAndStats('C:\\Users\\CINTHYA\\Documents\\md-links\\LIM008-fe-md-links\\test\\testing\\folder-2\\file-4.md', options);
 
 
