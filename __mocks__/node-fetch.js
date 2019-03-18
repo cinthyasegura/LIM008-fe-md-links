772e69ddc96d077c111b3094d063f1fd2d1bc048
cinthyasegura <cinthyaless@gmail.com>
2019-03-18 18:07:41 -0500

    se modifica node-fetch se agregan las url

--- __mocks__/node-fetch.js
+++ __mocks__/node-fetch.js
@@ -3,4 +3,10 @@ const fetchMock = require('fetch-mock').sandbox();
 Object.assign(fetchMock.config, nodeFetch, {
   fetch: nodeFetch
 });
+
+fetchMock
+      .mock('https://nodejs.org/api/path.html', 200)
+      .mock('https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e', 200)
+      
+
 module.exports = fetchMock;
